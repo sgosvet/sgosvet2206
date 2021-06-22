9421d84ab61a36e6852978bde78f35c714e0f739
sherrera <cmmii@hotmail.com>
2021-06-22 16:27:01 -0300

    Commit home app

--- pyawhere_test/urls.py
+++ pyawhere_test/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from home.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view()),
 ]
