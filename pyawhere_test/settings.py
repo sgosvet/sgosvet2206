635c2e02bb85ef4efaee7af4aea886267b2c16a2
sherrera <cmmii@hotmail.com>
2021-06-23 11:33:50 -0300

    Index file updated static files

--- pyawhere_test/settings.py
+++ pyawhere_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6=xg3ekd@wir@$1hm=b@dex#iev(d%!13b%c+cm476wym2vxtj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,11 +120,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Tratamiento de archivos MEDIA
 MEDIA_URL = '/media/'
